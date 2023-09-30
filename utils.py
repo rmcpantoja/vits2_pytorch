a2949beafde52fba34a62f257680b51099615782
rmcpantoja <angelitomateocedillo@gmail.com>
2023-09-29 23:59:17 -0500

    Add path to sabe the model.

--- utils.py
+++ utils.py
@@ -170,9 +170,10 @@ def get_hparams(init=True):
         help="JSON file for configuration",
     )
     parser.add_argument("-m", "--model", type=str, required=True, help="Model name")
+    parser.add_argument('-p', '--path', type=str, default="./logs", help='Directory to save the model')
 
     args = parser.parse_args()
-    model_dir = os.path.join("./logs", args.model)
+    model_dir = os.path.join(args.path, args.model)
 
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
